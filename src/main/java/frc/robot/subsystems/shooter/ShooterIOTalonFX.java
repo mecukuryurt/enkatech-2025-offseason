3c017f75de00e0b2d886827be98604eb9e850724
SeneSatka <senesatka@gmail.com>
2025-09-06 18:26:15 +0300

    feat: Shooter subsystem added.

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.shooter;
+
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+/** Add your docs here. */
+public class ShooterIOTalonFX implements ShooterIO {
+  VoltageOut voltageOut = new VoltageOut(0);
+  TalonFX motor;
+
+  public ShooterIOTalonFX(int CANID) {
+    this.motor = new TalonFX(CANID);
+  }
+
+  public void runAtVoltage(double voltage) {
+    motor.setControl(voltageOut.withOutput(voltage));
+  }
+}
