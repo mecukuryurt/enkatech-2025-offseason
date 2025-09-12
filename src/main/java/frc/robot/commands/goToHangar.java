ffaabacd5cc64595f7b9c573e41c2457327b6770
SeneSatka <senesatka@gmail.com>
2025-09-12 21:44:02 +0300

    refactor: wrist and arm pos added as variable

--- src/main/java/frc/robot/commands/goToHangar.java
+++ src/main/java/frc/robot/commands/goToHangar.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -9,7 +5,10 @@ import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.wrist.Wrist;
 
 public class goToHangar extends SequentialCommandGroup {
+  private double wristPos = -24;
+  private double armPos = -0.276;
+
   public goToHangar(Arm arm, Wrist wrist) {
-    addCommands(arm.goToPosition(-0.276), wrist.goToPosition(-24));
+    addCommands(arm.goToPosition(armPos), wrist.goToPosition(wristPos));
   }
 }
--- src/main/java/frc/robot/commands/shoot.java
+++ src/main/java/frc/robot/commands/shoot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -9,7 +5,10 @@ import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.wrist.Wrist;
 
 public class shoot extends SequentialCommandGroup {
+  private double wristPos = -35;
+  private double armPos = -0.335;
+
   public shoot(Arm arm, Wrist wrist) {
-    addCommands(arm.goToPosition(-0.335), wrist.goToPosition(-35));
+    addCommands(arm.goToPosition(armPos), wrist.goToPosition(wristPos));
   }
 }
