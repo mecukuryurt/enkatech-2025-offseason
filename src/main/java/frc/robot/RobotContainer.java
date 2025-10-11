d3ed17b7be3367ca3883261fe66b481306be55e4
mecukuryurt <mecukuryurt@gmail.com>
2025-10-11 08:34:29 +0300

    bir cisim yaklasiyor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,7 +13,11 @@
 
 package frc.robot;
 
+// import com.google.flatbuffers.Constants;
+
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -54,9 +58,12 @@ import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
@@ -78,18 +85,19 @@ public class RobotContainer {
   private SlewRateLimiter xLimiter = new SlewRateLimiter(4);
   private SlewRateLimiter yLimiter = new SlewRateLimiter(4);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     switch (Constants.currentMode) {
       case REAL:
         // Real robot, instantiate hardware IO implementations
-        drive =
-            new Drive(
-                new GyroIOPigeon2(),
-                new ModuleIOTalonFX(TunerConstants.FrontLeft),
-                new ModuleIOTalonFX(TunerConstants.FrontRight),
-                new ModuleIOTalonFX(TunerConstants.BackLeft),
-                new ModuleIOTalonFX(TunerConstants.BackRight));
+        drive = new Drive(
+            new GyroIOPigeon2(),
+            new ModuleIOTalonFX(TunerConstants.FrontLeft),
+            new ModuleIOTalonFX(TunerConstants.FrontRight),
+            new ModuleIOTalonFX(TunerConstants.BackLeft),
+            new ModuleIOTalonFX(TunerConstants.BackRight));
 
         gripper = new Gripper(new GripperIOTalonFX(Constants.GripperCANID));
         shooter = new Shooter(new ShooterIOTalonFX(Constants.ShooterCANID));
@@ -99,13 +107,13 @@ public class RobotContainer {
 
       case SIM:
         // Sim robot, instantiate physics sim IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIOSim(TunerConstants.FrontLeft),
-                new ModuleIOSim(TunerConstants.FrontRight),
-                new ModuleIOSim(TunerConstants.BackLeft),
-                new ModuleIOSim(TunerConstants.BackRight));
+        drive = new Drive(
+            new GyroIO() {
+            },
+            new ModuleIOSim(TunerConstants.FrontLeft),
+            new ModuleIOSim(TunerConstants.FrontRight),
+            new ModuleIOSim(TunerConstants.BackLeft),
+            new ModuleIOSim(TunerConstants.BackRight));
 
         gripper = new Gripper(new GripperIOSim());
         shooter = new Shooter(new ShooterIOSim());
@@ -115,13 +123,17 @@ public class RobotContainer {
 
       default:
         // Replayed robot, disable IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {});
+        drive = new Drive(
+            new GyroIO() {
+            },
+            new ModuleIO() {
+            },
+            new ModuleIO() {
+            },
+            new ModuleIO() {
+            },
+            new ModuleIO() {
+            });
 
         gripper = new Gripper();
         shooter = new Shooter();
@@ -129,6 +141,8 @@ public class RobotContainer {
         wrist = new Wrist();
         break;
     }
+
+    registerNamedCommands();
 
     // Set up auto routines
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
@@ -155,9 +169,11 @@ public class RobotContainer {
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
@@ -225,10 +241,9 @@ public class RobotContainer {
         .x()
         .onTrue(
             Commands.runOnce(
-                    () ->
-                        drive.setPose(
-                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
-                    drive)
+                () -> drive.setPose(
+                    new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
+                drive)
                 .ignoringDisable(true));
 
     driver.b().whileTrue(new approachToReef(drive));
@@ -240,7 +255,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return autoChooser.get();
+    return new PathPlannerAuto("volkswagen");
   }
 
   public void limelightPoseEstimatorMegaTag2() {
@@ -251,8 +266,7 @@ public class RobotContainer {
     Logger.recordOutput("ll-val", LimelightHelpers.getTX("limelight"));
 
     // Get the pose estimate
-    LimelightHelpers.PoseEstimate mt2 =
-        LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+    LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
 
     // Add it to your pose estimator
 
@@ -266,6 +280,19 @@ public class RobotContainer {
 
   public void logFiducial() {
     approachToReef.logFiducial();
+  }
+
+  private void registerNamedCommands() {
+    NamedCommands.registerCommand("Intake", gripper.runAtVoltage(Constants.GripperInTakeV));
+    NamedCommands.registerCommand("ShootRight", shooter.runAtVoltage(Constants.ShooterV));
+    NamedCommands.registerCommand("ShootLeft", shooter.runAtVoltage(-Constants.ShooterV));
+    NamedCommands.registerCommand("L1Position", new l1(arm, wrist));
+    NamedCommands.registerCommand(
+        "L1ShootAndAlgaeCommand", gripper.runAtVoltage(Constants.GripperBallV));
+    NamedCommands.registerCommand("goToHangar", new goToHangar(arm, wrist));
+    NamedCommands.registerCommand("goToShootRight", new shoot(arm, wrist));
+    NamedCommands.registerCommand("goToShootLeft", new leftShoot(arm, wrist));
+    NamedCommands.registerCommand("robotStartState", new start(arm, wrist));
   }
 
   // public double dist(){
